4d8970004dba3cfda8939bc4d016733d9d99efc5
Ekaterina <seamkate@gmail.com>
2021-02-10 19:30:34 +0300

    finished HTML, CSS. Change the item

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const itemBorder = document.querySelectorAll(".item__border");
+const weight = document.querySelectorAll(".weight");
+const item = document.querySelectorAll(".item");
+
